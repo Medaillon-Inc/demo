8096139c2fc2187c710fed2ab89c535e648fc99b
mehmeterenballi <mehmeterenballi@hotmail.com>
2023-01-22 01:40:51 +0300

    Add files via upload

--- styles/global.js
+++ styles/global.js
@@ -1,4 +1,3 @@
-
 import { StyleSheet } from 'react-native';
 
 export const globalStyles = StyleSheet.create({
@@ -31,6 +30,28 @@ export const images = {
         '3': require('../assets/modelPhotos/profilephotos/gigihadid.jpg'),
         '4': require('../assets/modelPhotos/profilephotos/adrianalima.jpg'),
         '5': require('../assets/modelPhotos/profilephotos/mervetaskin.jpg'),
+        '6': require('../assets/modelPhotos/profilephotos/jenselter.jpg'),
+        '7': require('../assets/modelPhotos/profilephotos/olyaabramovich.jpg'),
+        '8': require('../assets/modelPhotos/profilephotos/ozgebitmez.jpg'),
+    },
+};
+
+export const fanImages = {
+    photos: {
+        '0': require('../assets/modelPhotos/barbarapalvin.png'),
+        '1': require('../assets/modelPhotos/bellathorne.png'),
+        '2': require('../assets/modelPhotos/gigihadid.png'),
+        '3': require('../assets/modelPhotos/adrianalima.png'),
+        '4': require('../assets/modelPhotos/mervetaskin.png'),
+    },
+    profilePhotos: {
+        '0': require('../assets/fans/fanProfilePhotos/randomegirl.jpg'),
+        '1': require('../assets/fans/fanProfilePhotos/chad2.jpg'),
+        '2': require('../assets/fans/fanProfilePhotos/kurdishdayi.jpg'),
+        '3': require('../assets/fans/fanProfilePhotos/cilgindayi.jpg'),
+        '4': require('../assets/fans/fanProfilePhotos/incel.jpg'),
+        '5': require('../assets/fans/fanProfilePhotos/chad3.jpg'),
+        '6': require('../assets/fans/fanProfilePhotos/chad1.webp'),
     },
 };
 
@@ -42,5 +63,21 @@ export const notificationData = {
         '4': require('../assets/modelPhotos/profilephotos/olyaabramovich.jpg'),
         '5': require('../assets/modelPhotos/profilephotos/ozgebitmez.jpg'),
         '6': require('../assets/modelPhotos/profilephotos/tammyhembrow.jpg'),
+    },
+};
+
+export const challenges = {
+    challengePhotos: {
+        '1': require('../assets/challengePhotos/highfashion1.jpg'),
+        '2': require('../assets/challengePhotos/reddress1.jpg'),
+        '3': require('../assets/challengePhotos/cosplay2.jpg'),
+        '4': require('../assets/challengePhotos/blonde.jpg'),
+        '5': require('../assets/challengePhotos/bikini.jpg'),
+        '6': require('../assets/challengePhotos/fit1.webp'),
+        '7': require('../assets/challengePhotos/bikini1.jpg'),
+        '8': require('../assets/challengePhotos/elevator.png'),
+        '9': require('../assets/challengePhotos/dyehair.jpg'),
+        '10': require('../assets/challengePhotos/characterise.png'),
+        '11': require('../assets/challengePhotos/tattooedwoman.webp'),
     },
 };
