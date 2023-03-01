9926e5a7961b0143050d8f1b5fbbeff63e047d17
mehmeterenballi <mehmeterenballi@hotmail.com>
2023-03-01 03:26:31 +0300

    Add files via upload

---
+++ assets/fonts/fonts.js
@@ -0,0 +1,44 @@
+<<<<<<< HEAD
+export const brandHeader = {
+    fontWeight: "20",
+    fontFamily: "Billabong",
+}
+
+// export const bold = {
+//     fontWeight: "800",
+//     fontFamily: "SomeBoldFontFamily",
+// }
+
+// export const text = {
+//     fontSize: 12,
+//     lineHeight: 16,
+//     ...regular,
+// }
+
+// export const header = {
+//     fontSize: 16,
+//     lineHeight: 22,
+//     ...bold,
+=======
+export const brandHeader = {
+    fontWeight: "20",
+    fontFamily: "Billabong",
+}
+
+// export const bold = {
+//     fontWeight: "800",
+//     fontFamily: "SomeBoldFontFamily",
+// }
+
+// export const text = {
+//     fontSize: 12,
+//     lineHeight: 16,
+//     ...regular,
+// }
+
+// export const header = {
+//     fontSize: 16,
+//     lineHeight: 22,
+//     ...bold,
+>>>>>>> 5fce2ab (comments page added)
+// }
